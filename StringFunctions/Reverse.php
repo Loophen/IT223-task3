f5612ac6649846904cf94120e3400fbca747208b
Loophen <owlisglaring@gmail.com>
2026-02-06 07:42:07 +0800

    Add files via upload

---
+++ StringFunctions/Reverse.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SQL String Function</title>
+</head>
+<body>
+
+<?php
+include "../config/db.php";
+
+$sql = "SELECT REVERSE('SQL Tutorial') AS result;";
+$result = $conn->query($sql);
+
+if ($row = $result->fetch_assoc()) {
+    foreach ($row as $value) {
+        echo "<h3>Output:</h3>";
+        echo $value;
+    }
+}
+?>
+
+</body>
+</html>
---
+++ StringFunctions/SubStringIndex.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SQL String Function</title>
+</head>
+<body>
+
+<?php
+include "../config/db.php";
+
+$sql = "SELECT SUBSTRING_INDEX('www.w3schools.com', '.', 1) AS result;";
+$result = $conn->query($sql);
+
+if ($row = $result->fetch_assoc()) {
+    foreach ($row as $value) {
+        echo "<h3>Output:</h3>";
+        echo $value;
+    }
+}
+?>
+
+</body>
+</html>
